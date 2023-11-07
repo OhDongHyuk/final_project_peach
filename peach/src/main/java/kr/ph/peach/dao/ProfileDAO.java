6269076a62fc58144a405a6827b976f9ed5307fc
OhDongHyuk <daed124@naver.com>
2023-11-07 09:19:23 +0900

    11/07

--- peach/src/main/java/kr/ph/peach/dao/ProfileDAO.java
+++ peach/src/main/java/kr/ph/peach/dao/ProfileDAO.java
@@ -7,6 +7,7 @@ import org.apache.ibatis.annotations.Param;
 import kr.ph.peach.pagination.CriteriaProfile;
 import kr.ph.peach.vo.ProfileVO;
 
-public interface ProfileDao {
-
+public interface ProfileDAO
+{
+	
 }
--- peach/src/main/java/kr/ph/peach/service/ProfileServiceImp.java
+++ peach/src/main/java/kr/ph/peach/service/ProfileServiceImp.java
@@ -5,12 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import kr.ph.peach.dao.ProfileDao;
+import kr.ph.peach.dao.ProfileDAO;
 @Service
 public class ProfileServiceImp implements ProfileService{
 	
 	@Autowired
-	private ProfileDao profileDao;
+	private ProfileDAO profileDao;
 
 	
 }
