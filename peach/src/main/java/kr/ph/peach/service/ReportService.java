5c7f6ff9176fd0df3bcc969f79a0bfdb2a90b1da
OhDongHyuk <daed124@naver.com>
2023-11-21 17:02:54 +0900

    11221

---
+++ peach/src/main/java/kr/ph/peach/service/ReportService.java
@@ -0,0 +1,22 @@
+package kr.ph.peach.service;
+
+import java.util.List;
+
+import kr.ph.peach.pagination.Criteria;
+import kr.ph.peach.pagination.MemberCriteria;
+import kr.ph.peach.vo.ReportVO;
+
+public interface ReportService {
+
+
+	boolean insertReport(ReportVO reportVo);
+
+	List<ReportVO> getreportList(Criteria cri);
+
+	boolean deleteReport(ReportVO report);
+
+	int getTotalCount(Criteria cri);
+
+	
+	
+}
